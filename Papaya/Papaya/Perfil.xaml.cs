ecce88db8662995adcf2c9d8b0f12804bf456b99
Francisco-Freyre <61361950+Francisco-Freyre@users.noreply.github.com>
2022-01-05 16:05:34 -0600

    Cambios varios
    Boton para confirmar borrado de dieta y primer intento para subir imagenes al servidor desde la app

--- Papaya/Papaya/Perfil.xaml.cs
+++ Papaya/Papaya/Perfil.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,33 @@ namespace Papaya
         public Perfil()
         {
             InitializeComponent();
+        }
+
+        async void btnImage_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var imagen = await FilePicker.PickAsync(new PickOptions()
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Selecciona una imagen"
+            });
+
+            if (imagen != null)
+            {
+                var stream = await imagen.OpenReadAsync();
+                LblImg.Source = ImageSource.FromStream(() => stream);
+            }
+
+            MultipartFormDataContent formData = new MultipartFormDataContent();
+
+            MemoryStream m = new MemoryStream();
+
+            var f = new FileStream(imagen.FullPath, FileMode.Open, FileAccess.Read);
+
+            f.CopyTo(m);
+
+            HttpContent doc = new ByteArrayContent(m.ToArray());
+
+            formData.Add(doc, "imagen", imagen.FileName.Trim());
         }
     }
 }
